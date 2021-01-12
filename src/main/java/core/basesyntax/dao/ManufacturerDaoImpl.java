0060e87300c1766a410a3b59850643d5d6dbf8ac
YuraBondarenko <yuriybondarenko2001@gmail.com>
2021-01-12 13:38:00 +0200

    checkstyle fixed 2.0

--- src/main/java/core/basesyntax/dao/ManufacturerDaoImpl.java
+++ src/main/java/core/basesyntax/dao/ManufacturerDaoImpl.java
@@ -3,7 +3,6 @@ package core.basesyntax.dao;
 import core.basesyntax.db.Storage;
 import core.basesyntax.lib.Dao;
 import core.basesyntax.model.Manufacturer;
-
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.IntStream;
